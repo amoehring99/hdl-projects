81423cbb29645fd39e5532c0ed0f25036b8ce80c
Alexander Möhring <mail@amoehring.com>
2023-09-24 16:30:58 +0200

    resolving verilator lint errors

--- verilog/lib/seven_seg/seven_seg.sv
+++ verilog/lib/seven_seg/seven_seg.sv
@@ -1,3 +1,4 @@
+
 `include "seven_seg_pkg.sv" // compile the package
 
 // represnets a single digit on seven segment display
@@ -6,13 +7,18 @@ module seven_seg
   // input hex digit
   input  logic [3:0] din,
   // output seven bit signal for seven segment representation of input digit
-  output seven_seg_pkg::seven_seg_t [6:0] dout
+  output seven_seg_pkg::seven_seg_t dout
   );
 
-  import seven_seg_pkg::*;
+  // import seven_seg_pkg::*;
 
   always_comb begin
-    din <= ZERO.next(din);
+    //TODO implement function to convert input digit to seven segment representation
+    if(din == 0) begin
+      dout = seven_seg_pkg::ZERO;
+    end else begin
+      dout = seven_seg_pkg::ONE;
+    end
   end
   
 endmodule: seven_seg
--- verilog/seven_seg/ice40/top_seven_seg.sv
+++ verilog/seven_seg/ice40/top_seven_seg.sv
@@ -3,16 +3,17 @@
 module top_seven_seg
   #(parameter DIGIT_COUNT = 2)
   (
-  input logic                        [15:0] clk,
   input logic                        [15:0] din,
+  input logic                               clk,
   output seven_seg_pkg::seven_seg_t         dout
   );
 
-  import seven_seg_pkg::*;
+  // import seven_seg_pkg::*;
 
-  seven_seg_t display_value = DASH;
-  logic [3:0] current_value = din;
-  logic [3:0] digit_count = 0;
+  seven_seg_pkg::seven_seg_t display_value;
+  logic [15:0] current_din;
+  logic [3:0] current_value;
+  logic [3:0] digit_count;
 
   seven_seg seven_seg (
     .din(current_value),
@@ -22,13 +23,20 @@ module top_seven_seg
   always_ff @(posedge clk) begin
     
     dout <= display_value;
+    
+    if(digit_count == 0) begin
+      current_din <= din;
+      current_value <= current_din[3:0];
+    end
 
     if(digit_count < DIGIT_COUNT) begin
+      current_value <= current_din[3:0];
+      current_din <= current_din >> 4;
       digit_count <= digit_count + 1;
-      current_value <= (current_value >> 4);
     end else begin
       digit_count <= 0;
-      current_value <= din;
+      current_din <= din;
+      current_value <= current_din[3:0];
     end
     
   end
